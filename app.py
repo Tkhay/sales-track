87f8fe52962bebfe03e225b6f18aeaee2ea5119e
“Tieku <nanakhay21@gmail.com>
2026-03-27 09:27:19 +0000

    corrected some info

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ ITEM_DATA = {
     "MOBA Cloth (Funeral - R&B)": 80.0, 
     "MOBA Cloth (Ceremonial)": 80.0,
     "MOBA Cloth (General)": 80.0, 
-    "MOBA Cloth (Funeral - W&B)": 80.0,
+    "MOBA Cloth (Thanksgiving)": 80.0,
     "MOBA Tie": 200.0, 
     "MOBA Sticker (RECT-LONG)": 40.0,
     "Mfantsipim Sticker (SQ-SMALL)": 30.0, 
